5e0eaf12902fb382f8e452b176444b7e158e30fb
ksiim <makskrutchev@gmail.com>
2023-10-06 19:02:17 +0500

    refactored models (databases) and fixed bugs

--- models/databases.py
+++ models/databases.py
@@ -1,16 +1,20 @@
-from sqlalchemy.ext.asyncio import create_async_engine
-from sqlalchemy.ext.asyncio import AsyncAttrs
-from sqlalchemy.ext.asyncio import async_sessionmaker
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-engine = create_async_engine('sqlite+aiosqlite:///db.sqlite3')
-async_session = async_sessionmaker(engine, expire_on_commit=False)
+import os
+
+DB_NAME = 'tg_bot.db'
+DB_DIR = os.path.join(os.getcwd(), DB_NAME)
+
+engine = create_engine(f'sqlite:///{DB_DIR}')
+Session = sessionmaker(bind=engine)
+
+Base = declarative_base()
+
+import os
 
 
-class Base(AsyncAttrs, DeclarativeBase):
-    pass
-
-
-async def create_database():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
+def create_database():
+    if not os.path.exists(DB_NAME):
+        Base.metadata.create_all(bind=engine)
--- models/dbs/users.py
+++ models/dbs/users.py
@@ -1,48 +1,53 @@
-import asyncio
+from sqlalchemy import Column, Float, ForeignKey, Integer, PickleType, Text, Boolean, DateTime
+from sqlalchemy.ext.mutable import MutableList
+from sqlalchemy.orm import relationship
 
-from models.databases import Base, async_session
-
-from sqlalchemy import select
-from sqlalchemy.orm import Mapped
-from sqlalchemy.orm import mapped_column
+from models.databases import Base, Session
 
 
 class User(Base):
-    __tablename__ = "users"
+    __tablename__ = 'users'
 
-    id: Mapped[int] = mapped_column(primary_key=True)
-    telegram_id: Mapped[int]
-    full_name: Mapped[str]
-    username: Mapped[str]
+    id = Column(Integer, primary_key=True)
+    telegram_id = Column(Integer)
+    full_name = Column(Text)
+    username = Column(Text)
     
-    async def save(self):
-        async with async_session() as session:
-            await session.merge(self)
-            
-            await session.commit()
-    
-    @classmethod
-    async def get_user(cls, telegram_id):
-        async with async_session() as session:
-            stmt = select(cls).where(cls.telegram_id == telegram_id)
-            
-            user = await session.execute(stmt)
-            
-            return user.scalars().first()
-            
-    @classmethod
-    async def get_all_users(cls):
-        async with async_session() as session:
-            stmt = select(cls)
-            
-            users = await session.execute(stmt)
-            
-            return users.scalars().all()
+    def __init__(self, telegram_id, full_name, username):
+        self.telegram_id = telegram_id
+        self.full_name = full_name
+        self.username = username
         
-    async def delete(self):
-        async with async_session() as session:
-            
-            await session.delete(self)
-            
-            await session.commit()
-            
+    def save(self):
+        session = Session()
+        
+        session.merge(self)
+        
+        session.commit()
+        
+    @classmethod
+    def get_by_telegram_id(cls, telegram_id: int):
+        session = Session()
+        
+        user = session.query(cls).filter(cls.telegram_id == telegram_id).first()
+        return user
+    
+    @classmethod
+    def get_all_users(cls):
+        session = Session()
+        
+        users = session.query(cls).all()
+        return users
+    
+    @classmethod
+    def get_admins(cls):
+        session = Session()
+        
+        admins = session.query(cls).filter(cls.admin == True).all()
+        return admins
+        
+    def delete(self):
+        session = Session()
+        
+        session.delete(self)
+        session.commit()
